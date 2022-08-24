f93cd36021091a3d405b878398ddeac110d1e860
Anirudh M <82684635+BoildSoap@users.noreply.github.com>
2022-08-24 11:05:49 -0500

    added a different background, but have to fix crashes when no input is provided

--- app/src/main/java/com/example/mycalculatorapp/MainActivity.java
+++ app/src/main/java/com/example/mycalculatorapp/MainActivity.java
@@ -18,70 +18,86 @@ public class MainActivity extends AppCompatActivity {
         EditText number1ET = findViewById(R.id.textview_input1);
         EditText number2ET = findViewById(R.id.textview_input2);
         TextView numberSumTV = findViewById(R.id.output);
+        if (!(number1ET.getText().toString() == "" && number2ET.getText().toString() == "")) {
+            double num1 = Integer.parseInt((number1ET.getText().toString()));
+            double num2 = Integer.parseInt((number2ET.getText().toString()));
+            double sum = num1 + num2;
 
-        double num1 = Integer.parseInt((number1ET.getText().toString()));
-        double num2 = Integer.parseInt((number2ET.getText().toString()));
-        double sum = num1 + num2;
+            numberSumTV.setText("" + sum);
+        }else{
+                numberSumTV.setText("Please Enter a Number");
+            }
+        }
 
-        numberSumTV.setText("" + sum);
-    }
 
     public void findSub(View view) {
         EditText number1ET = findViewById(R.id.textview_input1);
         EditText number2ET = findViewById(R.id.textview_input2);
         TextView numberSumTV = findViewById(R.id.output);
+        if(!(number1ET.getText().toString()== "" && number2ET.getText().toString()== "")) {
+            double num1 = Integer.parseInt((number1ET.getText().toString()));
+            double num2 = Integer.parseInt((number2ET.getText().toString()));
+            double sub = num1 - num2;
+            numberSumTV.setText("" + sub);
+        }
+        else {
+            numberSumTV.setText("Please Enter a Number");
+        }
 
-        double num1 = Integer.parseInt((number1ET.getText().toString()));
-        double num2 = Integer.parseInt((number2ET.getText().toString()));
-        double sub = num1 - num2;
-
-        numberSumTV.setText("" + sub);
     }
 
     public void findMult(View view) {
         EditText number1ET = findViewById(R.id.textview_input1);
         EditText number2ET = findViewById(R.id.textview_input2);
         TextView numberSumTV = findViewById(R.id.output);
-
-        double num1 = Integer.parseInt((number1ET.getText().toString()));
-        double num2 = Integer.parseInt((number2ET.getText().toString()));
-        double mult = num1 * num2;
-
-        numberSumTV.setText("" + mult);
+        if(!(number1ET.getText().toString()== "" && number2ET.getText().toString()== "")) {
+            double num1 = Integer.parseInt((number1ET.getText().toString()));
+            double num2 = Integer.parseInt((number2ET.getText().toString()));
+            double mult = num1 * num2;
+            numberSumTV.setText("" + mult);
+        }else {
+            numberSumTV.setText("Please Enter a Number");
+        }
     }
 
     public void findDiv(View view) {
         EditText number1ET = findViewById(R.id.textview_input1);
         EditText number2ET = findViewById(R.id.textview_input2);
         TextView numberSumTV = findViewById(R.id.output);
-
-        double num1 = Integer.parseInt((number1ET.getText().toString()));
-        double num2 = Integer.parseInt((number2ET.getText().toString()));
-        double div = num1 / num2;
-
-        numberSumTV.setText("" + div);
+        if(!(number1ET.getText().toString()== "" && number2ET.getText().toString()== "")) {
+            double num1 = Integer.parseInt((number1ET.getText().toString()));
+            double num2 = Integer.parseInt((number2ET.getText().toString()));
+            double div = num1 / num2;
+            numberSumTV.setText("" + div);
+        }else {
+            numberSumTV.setText("Please Enter a Number");
+        }
     }
 
     public void findPwr(View view) {
         EditText number1ET = findViewById(R.id.textview_input1);
         EditText number2ET = findViewById(R.id.textview_input2);
         TextView numberSumTV = findViewById(R.id.output);
-
-        double num1 = Integer.parseInt((number1ET.getText().toString()));
-        double num2 = Integer.parseInt((number2ET.getText().toString()));
-        double pwr =  Math.pow(num1,num2);
-
-        numberSumTV.setText("" + pwr);
+        if(!(number1ET.getText().toString()== "" && number2ET.getText().toString()== "")) {
+            double num1 = Integer.parseInt((number1ET.getText().toString()));
+            double num2 = Integer.parseInt((number2ET.getText().toString()));
+            double pwr = Math.pow(num1, num2);
+            numberSumTV.setText("" + pwr);
+        }else{
+            numberSumTV.setText("Please Enter a Number");
+        }
     }
     public void findRoot(View view) {
         EditText number1ET = findViewById(R.id.textview_input1);
         EditText number2ET = findViewById(R.id.textview_input2);
         TextView numberSumTV = findViewById(R.id.output);
-
-        double num1 = Integer.parseInt((number1ET.getText().toString()));
-        double num2 = Integer.parseInt((number2ET.getText().toString()));
-        double root =  Math.pow(num1, 1/num2);
-
-        numberSumTV.setText("" + root);
+        if(!(number1ET.getText().toString()== "" && number2ET.getText().toString()== "")) {
+            double num1 = Integer.parseInt((number1ET.getText().toString()));
+            double num2 = Integer.parseInt((number2ET.getText().toString()));
+            double root = Math.pow(num1, 1 / num2);
+            numberSumTV.setText("" + root);
+        }else{
+            numberSumTV.setText("Please Enter a Number");
+        }
     }
 }
